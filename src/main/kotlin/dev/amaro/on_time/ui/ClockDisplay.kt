a5dad50ab7defbb260d9db5157338c4f8833e89c
Rodrigo Amaro <rodrigo@amaro.dev>
2022-06-09 18:49:35 -0500

    Fixing the Task Clock

--- src/main/kotlin/dev/amaro/on_time/ui/ClockDisplay.kt
+++ src/main/kotlin/dev/amaro/on_time/ui/ClockDisplay.kt
@@ -1,25 +1,23 @@
 package dev.amaro.on_time.ui
 
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import kotlinx.coroutines.delay
 import java.time.Duration
 import java.time.LocalDateTime
 
 @Composable
 fun ClockDisplay(initial: LocalDateTime) {
-    fun elapsed() : String {
-        return Duration.between(initial, LocalDateTime.now()).let{
+    val startedAt = remember { derivedStateOf { initial } }
+    fun elapsed(since: LocalDateTime) : String {
+        return Duration.between(since, LocalDateTime.now()).let{
             "${it.toHoursFormat()}:${it.toMinutesFormat()}"
         }
     }
-    val time = remember { mutableStateOf(elapsed()) }
+    val time = remember { mutableStateOf(elapsed(initial)) }
     LaunchedEffect(0) { // 3
         while (true) {
-            time.value = elapsed()
+            time.value = elapsed(startedAt.value)
             delay(1000)
         }
     }
@@ -31,5 +29,5 @@ fun Duration.toHoursFormat(): String  {
 }
 
 fun Duration.toMinutesFormat(): String  {
-    return toHoursPart().toString().padStart(2,'0')
+    return toMinutesPart().toString().padStart(2,'0')
 }
