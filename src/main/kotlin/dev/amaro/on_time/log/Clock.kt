2e58c3143f3b5a25cc3b19c32d15f8251b1b0429
Rodrigo Amaro <rodrigo@amaro.dev>
2022-11-08 15:51:36 -0500

    Fixing errors on tests with clock

--- src/main/kotlin/dev/amaro/on_time/log/Clock.kt
+++ src/main/kotlin/dev/amaro/on_time/log/Clock.kt
@@ -1,5 +1,6 @@
 package dev.amaro.on_time.log
 
+import dev.amaro.on_time.utilities.discardSecondsAndNanos
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
@@ -8,7 +9,7 @@ class Clock {
         private const val FORMAT = "yyyy-MM-dd HH:mm"
     }
 
-    fun now(): LocalDateTime = LocalDateTime.now().withNano(0).withSecond(0)
+    fun now(): LocalDateTime = LocalDateTime.now().discardSecondsAndNanos()
 
     fun nowStr(): String = now().format(DateTimeFormatter.ofPattern(FORMAT))
 }
